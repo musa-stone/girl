39f61f3c2753b99a6e95fbd79c770be035d8b76b
musa-stone <18337109503@163.com>
2018-08-29 15:11:59 +0800

    提交一个Spring boot 项目

---
+++ src/main/java/com/musa/girl/Girl.java
@@ -0,0 +1,40 @@
+package com.musa.girl;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Musa on 2018/8/25.
+ */
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class Girl {
+
+    private String cupSize;
+    private Integer age;
+    private String content;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/musa/girl/HelloController.java
@@ -0,0 +1,23 @@
+package com.musa.girl;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Musa on 2018/8/25.
+ */
+
+@RestController
+public class HelloController {
+
+    @Resource
+    private Girl girl;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String say() {
+        return girl.getContent();
+    }
+}
